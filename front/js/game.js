95baeba0df3a4fe411a6cf432de9cf2c7916faff
adrian61 <ad.kowalskI@gmail.com>
2020-06-22 18:06:35 +0200

    Changed config

--- front/js/game.js
+++ front/js/game.js
@@ -1,31 +1,7 @@
-var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: {y: 0},
-            debug: true
-        }
-    },
-    scene: {
-        preload: preload,
-        create: create
-    }
-};
+var Bomberman = Bomberman || {};
 
-
-var game = new Phaser.Game(config);
-
-function preload() {
-    this.load.setBaseURL('http://labs.phaser.io');
-
-    this.load.image('sky', 'assets/skies/space3.png');
-}
-
-function create() {
-    this.add.image(400, 300, 'sky');
-    var text = this.add.text(100, 100, 'Connect to the server to start.', { font: '32px Courier', fill: '#00ff00' });
-
-}
+var game = new Phaser.Game(240, 240, Phaser.CANVAS);
+game.state.add("BootState", new Bomberman.BootState());
+game.state.add("LoadingState", new Bomberman.LoadingState());
+game.state.add("TiledState", new Bomberman.TiledState());
+game.state.start("BootState", true, false, "js/assets.json", "TiledState");
