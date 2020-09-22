76617691896ba951e2fbea0e5e1167f143f11a0c
Adrian Ałaszewski <wns2148810@interia.pl>
2020-09-22 10:10:09 +0200

    Map as separate class, cleaner code

--- front/js/main.js
+++ front/js/main.js
@@ -1,4 +1,24 @@
 window.onload = () => {
     const screen = document.getElementById('display');
-    const game = new BomberGame(screen, {"x": 13, "y": 13});
+    const connectBtn = document.getElementById("connectBtn");
+    const disconnectBtn = document.getElementById("disconnectBtn");
+    const urlInput = document.getElementById("urlInput");
+    const nickInput = document.getElementById("nickInput");
+    const game = new BomberGame();
+    let serverConnection;
+
+    connectBtn.onclick = e => {
+        serverConnection = new ServerConnection(urlInput.value, nickInput.value);
+        if(serverConnection !== null) {
+            connectBtn.disabled = true;
+            disconnectBtn.disabled = false;
+            game.addMap(new GameMap(screen, {x: 11, y: 11}));
+        }
+    }
+
+    disconnectBtn.onclick = e => {
+        serverConnection.disconnect();
+        connectBtn.disabled = false;
+        disconnectBtn.disabled = true;
+    }
 }
